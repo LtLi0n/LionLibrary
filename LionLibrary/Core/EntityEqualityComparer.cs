3b2fd7cbef6aa28e2a14bc1a76d34aa3ad6aad28
Saulius Šaltenis <31516441+LtLi0n@users.noreply.github.com>
2020-11-14 23:14:45 +0200

    Added IEntityIdentity<KeyT> interface, to allow methods demand an entity id argument that must origin from the entity itself.

---
+++ LionLibrary/Core/EntityEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LionLibrary
+{
+    public class EntityEqualityComparer<EntityT, KeyT> : EqualityComparer<EntityT>
+        where EntityT : class, IEntity<EntityT, KeyT>
+        where KeyT : notnull, IEquatable<KeyT>, IComparable
+    {
+        public override bool Equals([AllowNull] EntityT x, [AllowNull] EntityT y) => Equals(x.Id, y.Id);
+        public override int GetHashCode([DisallowNull] EntityT obj) => obj.Id.GetHashCode();
+    }
+}
