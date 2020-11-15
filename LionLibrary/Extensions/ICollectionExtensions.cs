97620751d8c7eac9c9c4878db22c65b4566c41c1
Saulius Šaltenis <31516441+LtLi0n@users.noreply.github.com>
2020-11-16 01:18:42 +0200

    added IEnumerable.ForEach extension.

--- LionLibrary/Extensions/ICollectionExtensions.cs
+++ LionLibrary/Extensions/ICollectionExtensions.cs
@@ -2,11 +2,12 @@
 
 namespace LionLibrary
 {
-    public static class CollectionExtensions
+    public static class ICollectionExtensions
     {
         public static void CopyFrom<T>(this ICollection<T> collection, IEnumerable<T>? entities)
         {
             collection.Clear();
+
             if (entities != null)
             {
                 foreach (var entity in entities)
